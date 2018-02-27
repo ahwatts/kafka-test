e60d7a83c88a3ae102a70a96506b954629649c28
Andrew Watts <ahwatts@gmail.com>
2018-02-27 17:36:28 -0500

    Make the producer produce the same thing to each partition.
    So we can quickly check, message.value == message.offset.

--- producer.rb
+++ producer.rb
@@ -22,14 +22,21 @@ Phobos.configure({
   })
 srand(Time.now.to_i)
 
-done = 0
+PARTITION_TO_KEY = [ "7", "2", "1" ]
+done_by_partition = [ 0, 0, 0 ]
 
 loop do
-  messages = 0.upto(rand(5)).map do |n|
-    done += 1
-    { topic: "random_numbers", payload: done.to_s, key: done.to_s }
+  0.upto(2) do |p|
+    messages = 0.upto(rand(5)).map do |n|
+      done_by_partition[p] += 1
+      {
+        topic: "random_numbers",
+        payload: (done_by_partition[p] - 1).to_s,
+        key: PARTITION_TO_KEY[p],
+      }
+    end
+    FooProducer.producer.publish_list(messages)
   end
-  FooProducer.producer.publish_list(messages)
 
   sleep 10
 end
