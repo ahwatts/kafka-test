d6fc6e317c1ae94479d1592cbe32507049493bab
Andrew Watts <ahwatts@gmail.com>
2018-03-02 09:41:47 -0500

    Kill some old code.

--- consumer.rb
+++ consumer.rb
@@ -37,21 +37,3 @@ threads = 0.upto(2).map do |partition|
 end
 
 threads.each { |t| t.join }
-
-# client = Kafka::Client.new(seed_brokers: "kafka1:9092,kafka2:9092,kafka3:9092", client_id: "test_producer")
-
-# done = 0
-# last_printed = {}
-# client.each_message(topic: "random_numbers") do |message|
-#   p message
-
-#   if last_printed[message.partition].to_i < message.offset
-#     last_printed[message.partition] = message.offset
-#   end
-
-#   done += 1
-#   if done % 100 == 0
-#     p last_printed
-#     p client.last_offsets_for("random_numbers")
-#   end
-# end
